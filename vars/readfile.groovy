9f9e9fec9a8c9169392fc0b3214c0485fe6b17fa
demonhuahua <demon423@163.com>
2021-01-10 19:03:03 +0800

    Update readfile.groovy

--- vars/readfile.groovy
+++ vars/readfile.groovy
@@ -5,6 +5,7 @@ def read_yaml_file(yaml_file) {
 		
 	}else {
 		datas = readYaml text : yaml_file
+		writeYaml file: test.yml , data: datas
 	}
 	datas.each {
 		println ( it.key + " = " + it.value )
